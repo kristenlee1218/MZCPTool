c10fb236b0560c9685124b6b9870f8420350572e
kristen <1065200748@qq.com>
2023-01-30 14:18:14 +0800

    2023..01.30

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @author ：Kristen
+ * @date ：2023/1/15
+ * @description :
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
